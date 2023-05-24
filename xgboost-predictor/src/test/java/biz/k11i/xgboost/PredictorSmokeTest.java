990ca2f7cb00f78b284379009077ef7b81b4c901
Adam Valenta <adam.valenta@h2o.ai>
2023-05-24 15:43:42 +0200

    Make change in floating point operation order backwards compatible. The changes made in commit 9bed8be6c0394518cca13a8e88 to match xgboost 1.6 native predictions are not backwards compatible. Which caused fails in H2O-3 compatibility tests. This change is fixing it.

--- xgboost-predictor/src/test/java/biz/k11i/xgboost/PredictorSmokeTest.java
+++ xgboost-predictor/src/test/java/biz/k11i/xgboost/PredictorSmokeTest.java
@@ -11,19 +11,19 @@ public class PredictorSmokeTest {
     @Test
     public void shouldProvideEqualPredictionWithDifferentAPI_GBTree() throws IOException {
         Predictor predictor = new Predictor(getClass().getResourceAsStream("/prostate/boosterBytesProstateTree.bin"));
-        checkAPIForEachTreeImplementation(predictor, 63.56952667f /* obtain from xgboost version 1.2.0*/);
+        checkAPIForEachTreeImplementation(predictor, 63.56952667f /* obtain from xgboost version 1.2.0 - not affected with floating point operation order */);
     }
 
     @Test
     public void shouldProvideEqualPredictionWithDifferentAPI_GBLinear() throws IOException {
         Predictor predictor = new Predictor(getClass().getResourceAsStream("/prostate/boosterBytesProstateLinear.bin"));
-        checkAPIForEachTreeImplementation(predictor, 61.912136f /* obtain from xgboost version 1.2.0*/);
+        checkAPIForEachTreeImplementation(predictor, 61.912136f /* obtain from xgboost version 1.2.0 - not affected with floating point operation order */);
     }
 
     @Test
     public void shouldProvideEqualPredictionWithDifferentAPI_Dart() throws IOException {
         Predictor predictor = new Predictor(getClass().getResourceAsStream("/prostate/boosterBytesProstateDart.bin"));
-        checkAPIForEachTreeImplementation(predictor, 66.0059433f /* obtain from xgboost version 1.2.0*/);
+        checkAPIForEachTreeImplementation(predictor, 66.0059433f /* obtain from xgboost version 1.2.0 - not affected with floating point operation order */);
     }
 
     private void checkAPIForEachTreeImplementation(Predictor predictor, float expected) {
@@ -84,6 +84,33 @@ public class PredictorSmokeTest {
         Assert.assertEquals(1, prediction.length);
         Assert.assertEquals(1, predictionWithCustomBaseMargin.length);
         Assert.assertEquals(prediction[0] + 0.2f, predictionWithCustomBaseMargin[0], 0);
+    }
+
+    @Test
+    public void testThatPredictorCanSimulateDifferenceInFloatingPointOperationOrderAccordingToTheVersionInsideOfBinaryFile() throws IOException {
+        Predictor predictor12 = new Predictor(getClass().getResourceAsStream("/prostate/boosterBytesProstateTreeVersion12.bin"));
+        Predictor predictor16 = new Predictor(getClass().getResourceAsStream("/prostate/boosterBytesProstateTree.bin"));
+        float[] rowData = new float[] { 15.2f, 0};
+        FVec row = FVec.Transformer.fromArray(rowData, false);
+        float[] prediction12 = predictor12.predict(row);
+        float predictionSingle12 = predictor12.predictSingle(row);
+        float[] prediction16 = predictor16.predict(row);
+        float predictionSingle16 = predictor16.predictSingle(row);
+
+        float expected12 = 64.41069031f;
+        float expected16 = 64.41068268f;
+
+        Assert.assertEquals(expected12, prediction12[0], 0);
+        Assert.assertEquals(expected12, predictionSingle12, 0);
+        Assert.assertEquals(1, prediction12.length);
+        Assert.assertEquals(expected12, prediction12[0], 0);
+        Assert.assertEquals(expected12, predictionSingle12, 0);
+
+        Assert.assertEquals(expected16, prediction16[0], 0);
+        Assert.assertEquals(expected16, predictionSingle16, 0);
+        Assert.assertEquals(1, prediction16.length);
+        Assert.assertEquals(expected16, prediction16[0], 0);
+        Assert.assertEquals(expected16, predictionSingle16, 0);
     }
 
     //TODO add test that each parameter in each GradBooster implementation works properly
